54f99ab4a77257ca83d5c8c7c499644480f687c6
Umurzakov0204 <tomthompson0222@gmail.com>
2022-06-14 22:58:39 +0500

    style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import './About.css'
 import Header from "../Header/Header";
 
 function About() {
-    return(
-        <>
-            <Header />
-        <h1>About component</h1>
-        </>
+    return (
+        <section className={"about"}>
+            <Header/>
+            <section className={"block"}>
+                <section>
+                    <div className={"info__block"}>
+                        <h1>About me</h1>
+                        <p>Привет, меня зовут Али я Веб-разработчик <br/>
+                            Привет, меня зовут Али я Веб-разработчик <br/>
+                            Привет, меня зовут Али я Веб-разработчик <br/>
+                            Привет, меня зовут Али я Веб-разработчик <br/>
+                            Привет, меня зовут Али я Веб-разработчик <br/>
+                            Привет, меня зовут Али я Веб-разработчик <br/>
+                        </p>
+                    </div>
+                </section>
+                <section className={"photo"}>
+                    <div></div>
+                </section>
+            </section>
+        </section>
     );
 }
 
